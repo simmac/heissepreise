9395456be881ca83c756d4da6c616950d3f94c69
simmac <simmac@users.noreply.github.com>
2026-03-27 15:43:24 +0100

    Disable GitHub Pages generation check
    Commented out the conditional check for repository name.

--- pages.sh
+++ pages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 repository_name=${GITHUB_REPOSITORY#*/}
-if [[ $repository_name == *.github.io ]]; then
-  echo "Name ends with github.io"
-else
-  echo "Name does not end with github.io, not generating pages"
-  exit
-fi
+#if [[ $repository_name == *.github.io ]]; then
+#  echo "Name ends with github.io"
+#else
+#  echo "Name does not end with github.io, not generating pages"
+#  exit
+#fi
 
 npm install
 node pages.js
